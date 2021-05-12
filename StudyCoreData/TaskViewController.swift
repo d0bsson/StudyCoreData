ee9a25cfdf04489ec7da97e95cb87ec6f5b88cdb
David <ber.dev@yandex.ru>
2021-05-12 22:51:44 +0300

    create two buttons

--- StudyCoreData/TaskViewController.swift
+++ StudyCoreData/TaskViewController.swift
@@ -8,10 +8,88 @@
 import UIKit
 
 class TaskViewController: UIViewController {
+    
+    private lazy var taskTextField: UITextField = {
+       
+        let textField = UITextField()
+        textField.placeholder = "New task"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(
+            displayP3Red: 21/255,
+            green: 101/255,
+            blue: 192/255,
+            alpha: 1
+        )
+        button.setTitle("Save Task", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(save), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setup(subviews: taskTextField, saveButton, cancelButton)
+        setConstrains()
     }
+    
+    private func setup(subviews: UIView...) {
+        subviews.forEach { (subview) in
+            view.addSubview(subview)
+        }
+    }
+    
+    private func setConstrains() {
+        
+        taskTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            taskTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80),
+            taskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: taskTextField.topAnchor, constant: 80),
+            saveButton.leadingAnchor.constraint(equalTo: taskTextField.leadingAnchor, constant: 0),
+            saveButton.trailingAnchor.constraint(equalTo: taskTextField.trailingAnchor, constant: 0)
+        ])
+        
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: saveButton.topAnchor, constant: 80),
+            cancelButton.leadingAnchor.constraint(equalTo: saveButton.leadingAnchor, constant: 0),
+            cancelButton.trailingAnchor.constraint(equalTo: saveButton.trailingAnchor, constant: 0)
+        ])
+        
+    }
+    
+    @objc private func save() {
+        dismiss(animated: true)
+    }
+    
+    @objc private func cancel() {
+        dismiss(animated: true)
+    }
+    
 }
 
 
